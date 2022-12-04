87b967a304d74c74671d0b9e059c2fedeabfe00b
Sabrinax14 <98049249+Sabrinax14@users.noreply.github.com>
2022-12-04 20:59:10 +0000

    updates

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ const messenger = function (msg, parentElement, time = 3){
 // creating setting for the reset using function
 
 const restValues = function () {
-    setTime.value = 30;
+    setTime.value = 45;
     setRest.value = 15;
     setRounds.value = 5;
 };
@@ -108,11 +108,101 @@ const stopTraining = function () {
     isRunning =false;
     time = setTime.value;
     rounds = setRounds.value;
-    restTime =setRest.value;
+    restTime = setRest.value;
     startBtn.innerHTML = 'Start';
     timerEl.innerHTML = sechuman(time);
     roundEl.innerHTML = rounds;
     circleTimer.style.background ='white';
     circleTimer.style.color ='black';
-    cmdEl.innerHTML = 'Get Ready !';
+    cmdEl.innerHTML = 'Get Ready!';
+    // changes the cmdel to 'Get Ready'
 };
+
+// creating the timer function using seinterval
+function timer(_stopFunction){
+    audioPlayer(); // plays audio
+    // starts training
+    isRunning = true;
+    circleTimer.style.background = green;
+    circleTimer.style.color = 'white';
+    roundEl.innerHTML = rounds;
+    cmdEl.innerHTML ='Work!';
+    interval = setInterval(() => {
+        timerEl.innerHTML = sechuman(time);
+        time--;
+        time < 0 ? audioPlayer() :"";
+
+        // if the rounds  finish
+        if (time < 0){
+            clearInterval(interval);
+            rounds--;
+
+            // rounds finish but exercie time is not finished
+            if (rounds > 0) {
+                circleTimer.style.background = orange;
+                circleTimer.style.color = 'white';
+                cmdEl.innerHTML = 'Rest!';
+                roundEl.innerHTML = rounds;
+                pauseInterval = setInterval(() => {
+                    timerEl.innerHTML = sechuman(restTime)
+                    restTime--;
+                    // rest times up
+                    if (restTime < 0 ) {
+                        clearInterval(pauseInterval);
+                        restTime = setRest.value;
+                        time = setTime.value;
+                        return timer();
+                    }
+                }, 1000 );
+            } // training finishes
+            if (rounds === 0){
+                stopTraining();
+                cmdEl.innerHTML = 'Great Job!';
+            }
+        }
+    },1000);
+}
+
+// make the pause function to pause timer ?? or addevenlitenser
+
+// add event listenr to start, rest and pause buttn
+startBtn.addEventListener('click', () => {
+    !isRunning ? timer(stopTraining) : stopTraining();
+    !isRunning ? (startBtn.innerHTML = 'Start') : (startBtn.innerHTML = 'Finish');
+});
+
+resetBtn.addEventListener('click', () => {
+    if(!isRunning){
+        restValues();
+        timerSettings();
+    }
+});
+
+pauseBtn.addEventListener('click', () =>{
+    if (paused === undefined){
+        return;
+    }
+    if (paused){
+        pause = false;
+        initaltimes = setInterval(interval, 1000);
+        pauseBtn.textContent = " ";
+        pauseBtn.classList.remove('Resume')
+    } else {
+     clearInterval(initaltimes);
+     pauseBtn.textContent =" ";
+     pauseBtn.classList.add('Resume')
+     pauseBtn.style.color = 'white';
+     paused = true;
+    }
+});
+
+circleTimer.addEventListener('click', () => {
+    !isRunning ? timer(stopTraining) : stopTraining();
+    !isRunning ? (startBtn.innerHTML = 'Start') : (startBtn.innerHTML = 'Finish');
+});
+
+// sound checkbox
+soundBox.addEventListener('change', () => {
+    console.log(soundBox.checked);
+    soundBox.checked ? (soundOnOff = true) : (soundOnOff = false);
+});
