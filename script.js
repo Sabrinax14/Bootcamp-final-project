1d4eb12810ce2d81c941550d8affd24e7360b436
Sabrinax14 <98049249+Sabrinax14@users.noreply.github.com>
2022-12-14 22:13:06 +0000

    Fixed pause button and update function.

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// declare variables
-// stylying elements for timer circle
-
 const green = '#2ee22e';
 const orange = '#ffae1b';
 
@@ -32,10 +29,12 @@ const circleTimer = document.querySelector('.timer-container');
 const setAlwaysOn = document.querySelector('#screen-on');
 const soundBox = document.querySelector('#soundSwitch');
 
-const formInputs = document.querySelectorAll('#form_input');
+const formInputs = document.querySelectorAll('.form_input');
 const setTime = document.querySelector('#setTime');
 const setRest = document.querySelector('#setRest');
 const setRounds = document.querySelector('#setRound');
+
+let resumeBtn = document.getElementById('Resume');
 
 // creating a msg el inside to circle timer 
 const messenger = function (msg, parentElement, time = 3){
@@ -60,6 +59,7 @@ const messenger = function (msg, parentElement, time = 3){
 // creating setting for the reset using function
 
 const restValues = function () {
+    // console.log('restValues');
     setTime.value = 45;
     setRest.value = 15;
     setRounds.value = 5;
@@ -78,16 +78,29 @@ let screenAlwaysOn = false;
 
 // make a function to formating the time in timer
 function sechuman(seconds) {
-    sec = seconds % 60;
-    min = parseInt(seconds / 60);
-    if (sec.toString().length == 1) {
-        sec = '0' + sec ;
-    }
-    return min + ':' + sec;
-}
+    // console.log('sechuman');
+    // console.log(pauseBtn.textContent)
+    
+        //this is when paused
 
+     if (pauseBtn.textContent === 'Pause') {
+        //this is not paused
+        sec = seconds % 60;
+        min = parseInt(seconds / 60);
+        // console.log('pain');
+   if (sec.toString().length == 1) 
+        sec = '0' + sec ;
+    
+    return min + ':' + sec;
+  
+
+}else if (pauseBtn.textContent === 'Paused'){
+
+}
+}
 //  adding evenetlistener and setting timer 
 const timerSettings = function () {
+    // console.log('timerSettings');
     time = setTime.value;
     rounds = setRounds.value;
     restTime = setRest.value;
@@ -96,14 +109,37 @@ const timerSettings = function () {
 };
 
 // set timmers
+  
 function update(){
-    formInputs.addEventListener('change', () => {
+    // console.log('update', formInputs, setTime.timerEl);
+    formInputs.forEach((inputs) =>{
         
-     timerSettings();
-    });  
+        // allows inout values to be updated.
+        console.log(inputs);
+        if (time !== formInputs[0].value ) {
+           time = formInputs[0].value; 
+            timerEl.innerHTML = sechuman(formInputs[0].value );
+          if(restTime !== formInputs[1].value){
+            restTime = formInputs[1].value ;
+            timerEl.innerHTML = sechuman(formInputs[1].value );
+           if (rounds !== formInputs[2].value){
+            rounds = formInputs[2].value ;
+            roundEl.innerHTML = formInputs[2].value ;
+           } 
+          }
+          timer();
+        } else if( timerSettings()){
+
+        };
+
+        
+    //    timerSettings();
+ });  
+
 };
 
 const stopTraining = function () {
+    // console.log('stopTraining');
     clearInterval(pauseInterval);
     clearInterval(interval);
     isRunning = false;
@@ -111,16 +147,31 @@ const stopTraining = function () {
     rounds = setRounds.value;
     restTime = setRest.value;
     startBtn.innerHTML = 'Start';
-    timerEl.innerHTML = sechuman(time);
+    timerEl.innerHTML = sechuman(time); //might not be needed
     roundEl.innerHTML = rounds;
     circleTimer.style.background ='white';
     circleTimer.style.color ='black';
     cmdEl.innerHTML = 'Get Ready!';
     // changes the cmdel to 'Get Ready'
 };
-
-// creating the timer function using seinterval
-function timer(_stopFunction){
+  // creating the timer function using seinterval /_stopFunction
+function timer(){
+    
+    if (currentTime === 0){
+      
+    } else if ( currentTime > 0){
+        time = currentTime
+    }
+//    console.log(formInputs[0].value)
+    // console.log('timer');
+    // console.log(pauseBtn.textContent,'timerel')
+    if (pauseBtn.textContent === 'Paused'){
+        // console.log('Paused');
+        clearInterval(interval)
+        // console.log(interval)
+    } else if (pauseBtn.textContent === 'Pause')  {
+     
+//    console.log('running');
     audioPlayer(); // plays audio
     // starts training
     isRunning = true;
@@ -128,27 +179,32 @@ function timer(_stopFunction){
     circleTimer.style.color = 'white';
     roundEl.innerHTML = rounds;
     cmdEl.innerHTML ='Work!';
+    /**this is problem */
     interval = setInterval(() => {
-        timerEl.innerHTML = sechuman(time);
+        // console.log('interval')
+       timerEl.innerHTML = sechuman(time);
         time--;
         time < 0 ? audioPlayer() :"";
 
         // if the rounds  finish
         if (time < 0){
+            // console.log('time');
             clearInterval(interval);
             rounds--;
 
             // rounds finish but exercie time is not finished
             if (rounds > 0) {
+                // console.log('round');
                 circleTimer.style.background = orange;
                 circleTimer.style.color = 'white';
                 cmdEl.innerHTML = 'Rest!';
                 roundEl.innerHTML = rounds;
                 pauseInterval = setInterval(() => {
-                    timerEl.innerHTML = sechuman(restTime);
+                 timerEl.innerHTML = sechuman(restTime);
                     restTime--;
                     // rest times up
                     if (restTime < 0 ) {
+                        // console.log('restTime');
                         clearInterval(pauseInterval);
                         restTime = setRest.value;
                         time = setTime.value;
@@ -157,22 +213,24 @@ function timer(_stopFunction){
                 }, 1000 );
             } // training finishes
             if (rounds === 0){
+                // console.log('stopS')
                 stopTraining();
                 cmdEl.innerHTML = 'Great Job!';
             }
         }
     },1000);
-}
+} }
 
 
 // // add event listenr to start, rest and pause buttn
 startBtn.addEventListener('click', () => {
-    !isRunning ? timer(stopTraining) : stopTraining();
+    // console.log('start', isRunning)
+    !isRunning ? update() : stopTraining();
     !isRunning ? (startBtn.innerHTML = 'Start') : (startBtn.innerHTML = 'Finish');
-});
+},true);
 
 resetBtn.addEventListener('click', () => {
-    if(!isRunning){
+    if(isRunning){
         restValues();
         timerSettings();
     }
@@ -180,26 +238,55 @@ resetBtn.addEventListener('click', () => {
 
 // create event listenr for the pause button - when pressed will pasuse the timer and transform to resume button.
 // resume button will start up the timer again and transform back to pause button.
+let currentTime = 0
+function pauseTimer (){
+    
+        pause = true;
+        currentTime = timerEl.innerHTML;
+        // console.log(currentTime, timerEl.innerHTML);
+        timerEl.innerHTML = 'Paused';
+     
+        isRunning = false;
+       //  let initaltimes = setTimeout(timer(), 1000);
+        pauseBtn.innerHTML = "Paused";
+       //  pauseBtn.classList.remove('Resume')
+       timer();
+       }  
+
+function Resume(){
+    
+        // clearInterval(initaltimes);
+        // console.log(currentTime)
+        isRunning = true;
+        timerEl.innerHTML = sechuman(currentTime);
+        pauseBtn.innerHTML = "Pause";
+        // pauseBtn.classList.add('Resume')
+         
+    
+       // pauseBtn.style.color = 'white';
+        paused = false;
+        // console.log(pauseBtn);
+        timer();
+}
 
 pauseBtn.addEventListener('click', () =>{
-  if (paused = pauseBtn){
-     return; 
-    } 
-    if (paused){
-     pause = false;
-     initaltimes = setTimeout(timer(), 1000);
-     pauseBtn.textContent = " ";
-     pauseBtn.classList.remove('Resume')
-    } else {
-    clearInterval(initaltimes);
-    pauseBtn.textContent =" ";
-    pauseBtn.classList.add('Resume')
-    pauseBtn.style.color = 'white';
-    paused = true;
-}
-});
+//   console.log('Pause', pauseBtn.innerHTML);
+   resumeBtn.style.transform = "scale(1)";
+    pauseTimer();
+  
+ 
     
-console.log('pause');
+},true);
+    
+
+resumeBtn.addEventListener('click', () =>{
+    // console.log('Resume', resumeBtn.innerHTML);
+    resumeBtn.style.transform = "scale(0)";
+    Resume();
+
+   
+      
+},true);
 
 circleTimer.addEventListener('click', () => {
     !isRunning ? timer(stopTraining) : stopTraining();
@@ -208,7 +295,6 @@ circleTimer.addEventListener('click', () => {
 
 // sound checkbox
 soundBox.addEventListener('change', () => {
-    console.log(soundBox.checked);
+    // console.log(soundBox.checked);
     soundBox.checked ? (soundOnOff = true) : (soundOnOff = false);
 });
-
